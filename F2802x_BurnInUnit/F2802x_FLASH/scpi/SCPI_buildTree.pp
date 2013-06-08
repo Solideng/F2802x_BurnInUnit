a6c46f44ff091d86f5e73a1154b07f50038922e2
Toby <userMe@myServer.fake>
2013-06-08 13:53:00 +0100

    Added flash obj to index

---
+++ F2802x_BurnInUnit/F2802x_FLASH/scpi/SCPI_buildTree.pp
@@ -0,0 +1,119 @@
+# FIXED
+
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_buildTree.c
+scpi/SCPI_buildTree.obj: ../scpi/scpi.h
+scpi/SCPI_buildTree.obj: ../scpi/../Common.h
+scpi/SCPI_buildTree.obj: ../scpi/../Settings.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/development_kits/~SupportFiles/F2802x_headers/PeripheralHeaderIncludes.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_Adc.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_DevEmu.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_Comp.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_CpuTimers.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_ECap.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_EPwm.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_Gpio.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_I2c.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_NmiIntrupt.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_PieCtrl.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_PieVect.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_Spi.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_Sci.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_SysCtrl.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_XIntrupt.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_common/include/DSP2802x_EPWM_defines.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_common/include/IQMathLib.h
+scpi/SCPI_buildTree.obj: C:/ti/ccsv5/tools/compiler/c2000_6.1.3/include/limits.h
+scpi/SCPI_buildTree.obj: ../scpi/../SQMath.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/libs/app_libs/digital_power/f2802x_v3.1/include/DPlib.h
+scpi/SCPI_buildTree.obj: c:/ti/controlSUITE/libs/dsp/SGEN/v101/include/sgen.h
+scpi/SCPI_buildTree.obj: ../scpi/../StateMachine.h
+scpi/SCPI_buildTree.obj: ../scpi/../I2c.h
+scpi/SCPI_buildTree.obj: ../scpi/../MacroNets.h
+scpi/SCPI_buildTree.obj: ../scpi/../Timers.h
+scpi/SCPI_buildTree.obj: ../scpi/../Adc.h
+scpi/SCPI_buildTree.obj: ../scpi/../Pwm.h
+scpi/SCPI_buildTree.obj: ../scpi/../Cntl.h
+scpi/SCPI_buildTree.obj: ../scpi/../SlewControl.h
+scpi/SCPI_buildTree.obj: ../scpi/../SineGen.h
+scpi/SCPI_buildTree.obj: ../scpi/../PhaseCtrl.h
+scpi/SCPI_buildTree.obj: ../scpi/../tmp.h
+scpi/SCPI_buildTree.obj: ../scpi/../FanEn.h
+scpi/SCPI_buildTree.obj: ../scpi/../BstEn.h
+scpi/SCPI_buildTree.obj: C:/ti/ccsv5/tools/compiler/c2000_6.1.3/include/stdlib.h
+scpi/SCPI_buildTree.obj: C:/ti/ccsv5/tools/compiler/c2000_6.1.3/include/linkage.h
+scpi/SCPI_buildTree.obj: C:/ti/ccsv5/tools/compiler/c2000_6.1.3/include/stdlibf.h
+scpi/SCPI_buildTree.obj: C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_Device.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_msgEPCtrl.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_regs.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_specificRegs.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_string.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_buildTree.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_commonCmds.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_requiredCmds.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_specificCmds.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_serialEmulation.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_inputBuffer.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_outputQueue.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_lexer.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_parser.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_execCtrl.h
+scpi/SCPI_buildTree.obj: ../scpi/SCPI_respFmtr.h
+
+../scpi/SCPI_buildTree.c: 
+../scpi/scpi.h: 
+../scpi/../Common.h: 
+../scpi/../Settings.h: 
+C:/ti/controlSUITE/development_kits/~SupportFiles/F2802x_headers/PeripheralHeaderIncludes.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_Adc.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_DevEmu.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_Comp.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_CpuTimers.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_ECap.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_EPwm.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_Gpio.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_I2c.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_NmiIntrupt.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_PieCtrl.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_PieVect.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_Spi.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_Sci.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_SysCtrl.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_XIntrupt.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_common/include/DSP2802x_EPWM_defines.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_common/include/IQMathLib.h: 
+C:/ti/ccsv5/tools/compiler/c2000_6.1.3/include/limits.h: 
+../scpi/../SQMath.h: 
+C:/ti/controlSUITE/libs/app_libs/digital_power/f2802x_v3.1/include/DPlib.h: 
+c:/ti/controlSUITE/libs/dsp/SGEN/v101/include/sgen.h: 
+../scpi/../StateMachine.h: 
+../scpi/../I2c.h: 
+../scpi/../MacroNets.h: 
+../scpi/../Timers.h: 
+../scpi/../Adc.h: 
+../scpi/../Pwm.h: 
+../scpi/../Cntl.h: 
+../scpi/../SlewControl.h: 
+../scpi/../SineGen.h: 
+../scpi/../PhaseCtrl.h: 
+../scpi/../tmp.h: 
+../scpi/../FanEn.h: 
+../scpi/../BstEn.h: 
+C:/ti/ccsv5/tools/compiler/c2000_6.1.3/include/stdlib.h: 
+C:/ti/ccsv5/tools/compiler/c2000_6.1.3/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/c2000_6.1.3/include/stdlibf.h: 
+C:/ti/controlSUITE/device_support/f2802x/v128/DSP2802x_headers/include/DSP2802x_Device.h: 
+../scpi/SCPI_msgEPCtrl.h: 
+../scpi/SCPI_regs.h: 
+../scpi/SCPI_specificRegs.h: 
+../scpi/SCPI_string.h: 
+../scpi/SCPI_buildTree.h: 
+../scpi/SCPI_commonCmds.h: 
+../scpi/SCPI_requiredCmds.h: 
+../scpi/SCPI_specificCmds.h: 
+../scpi/SCPI_serialEmulation.h: 
+../scpi/SCPI_inputBuffer.h: 
+../scpi/SCPI_outputQueue.h: 
+../scpi/SCPI_lexer.h: 
+../scpi/SCPI_parser.h: 
+../scpi/SCPI_execCtrl.h: 
+../scpi/SCPI_respFmtr.h: 
---
+++ F2802x_BurnInUnit/F2802x_FLASH/scpi/subdir_vars.mk
@@ -0,0 +1,108 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../scpi/SCPI_buildTree.c \
+../scpi/SCPI_commonCmds.c \
+../scpi/SCPI_execCtrl.c \
+../scpi/SCPI_inputBuffer.c \
+../scpi/SCPI_lexer.c \
+../scpi/SCPI_msgEPCtrl.c \
+../scpi/SCPI_outputQueue.c \
+../scpi/SCPI_parser.c \
+../scpi/SCPI_regs.c \
+../scpi/SCPI_requiredCmds.c \
+../scpi/SCPI_respFmtr.c \
+../scpi/SCPI_serialEmulation.c \
+../scpi/SCPI_specificCmds.c \
+../scpi/SCPI_specificRegs.c \
+../scpi/SCPI_string.c 
+
+OBJS += \
+./scpi/SCPI_buildTree.obj \
+./scpi/SCPI_commonCmds.obj \
+./scpi/SCPI_execCtrl.obj \
+./scpi/SCPI_inputBuffer.obj \
+./scpi/SCPI_lexer.obj \
+./scpi/SCPI_msgEPCtrl.obj \
+./scpi/SCPI_outputQueue.obj \
+./scpi/SCPI_parser.obj \
+./scpi/SCPI_regs.obj \
+./scpi/SCPI_requiredCmds.obj \
+./scpi/SCPI_respFmtr.obj \
+./scpi/SCPI_serialEmulation.obj \
+./scpi/SCPI_specificCmds.obj \
+./scpi/SCPI_specificRegs.obj \
+./scpi/SCPI_string.obj 
+
+C_DEPS += \
+./scpi/SCPI_buildTree.pp \
+./scpi/SCPI_commonCmds.pp \
+./scpi/SCPI_execCtrl.pp \
+./scpi/SCPI_inputBuffer.pp \
+./scpi/SCPI_lexer.pp \
+./scpi/SCPI_msgEPCtrl.pp \
+./scpi/SCPI_outputQueue.pp \
+./scpi/SCPI_parser.pp \
+./scpi/SCPI_regs.pp \
+./scpi/SCPI_requiredCmds.pp \
+./scpi/SCPI_respFmtr.pp \
+./scpi/SCPI_serialEmulation.pp \
+./scpi/SCPI_specificCmds.pp \
+./scpi/SCPI_specificRegs.pp \
+./scpi/SCPI_string.pp 
+
+C_DEPS__QUOTED += \
+"scpi\SCPI_buildTree.pp" \
+"scpi\SCPI_commonCmds.pp" \
+"scpi\SCPI_execCtrl.pp" \
+"scpi\SCPI_inputBuffer.pp" \
+"scpi\SCPI_lexer.pp" \
+"scpi\SCPI_msgEPCtrl.pp" \
+"scpi\SCPI_outputQueue.pp" \
+"scpi\SCPI_parser.pp" \
+"scpi\SCPI_regs.pp" \
+"scpi\SCPI_requiredCmds.pp" \
+"scpi\SCPI_respFmtr.pp" \
+"scpi\SCPI_serialEmulation.pp" \
+"scpi\SCPI_specificCmds.pp" \
+"scpi\SCPI_specificRegs.pp" \
+"scpi\SCPI_string.pp" 
+
+OBJS__QUOTED += \
+"scpi\SCPI_buildTree.obj" \
+"scpi\SCPI_commonCmds.obj" \
+"scpi\SCPI_execCtrl.obj" \
+"scpi\SCPI_inputBuffer.obj" \
+"scpi\SCPI_lexer.obj" \
+"scpi\SCPI_msgEPCtrl.obj" \
+"scpi\SCPI_outputQueue.obj" \
+"scpi\SCPI_parser.obj" \
+"scpi\SCPI_regs.obj" \
+"scpi\SCPI_requiredCmds.obj" \
+"scpi\SCPI_respFmtr.obj" \
+"scpi\SCPI_serialEmulation.obj" \
+"scpi\SCPI_specificCmds.obj" \
+"scpi\SCPI_specificRegs.obj" \
+"scpi\SCPI_string.obj" 
+
+C_SRCS__QUOTED += \
+"../scpi/SCPI_buildTree.c" \
+"../scpi/SCPI_commonCmds.c" \
+"../scpi/SCPI_execCtrl.c" \
+"../scpi/SCPI_inputBuffer.c" \
+"../scpi/SCPI_lexer.c" \
+"../scpi/SCPI_msgEPCtrl.c" \
+"../scpi/SCPI_outputQueue.c" \
+"../scpi/SCPI_parser.c" \
+"../scpi/SCPI_regs.c" \
+"../scpi/SCPI_requiredCmds.c" \
+"../scpi/SCPI_respFmtr.c" \
+"../scpi/SCPI_serialEmulation.c" \
+"../scpi/SCPI_specificCmds.c" \
+"../scpi/SCPI_specificRegs.c" \
+"../scpi/SCPI_string.c" 
+
+
