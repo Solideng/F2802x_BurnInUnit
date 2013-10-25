cd8dc0b6816ac6bac97846c009bc3dc0c0221289
Toby <userMe@myServer.fake>
2013-10-25 16:11:33 +0100

    Corrected documentation

--- F2802x_BurnInUnit/SlewControl.h
+++ F2802x_BurnInUnit/SlewControl.h
@@ -14,14 +14,14 @@ extern void updateLoadSlew (void);
 
 /** Sets the slew target for the specified load.
  * @param[in]	load	Specifies the load the setting is to be applied to.
- * @param[in]	trgt	Specifies the value of the slew target to be applied (amps or volts).
+ * @param[in]	trgt	Specifies the value of the slew target to be applied (amps).
  * @return				Error status.
  */
 extern Uint16 setLoadSlewTarget (loadStage load, float32 trgt);
 
 /** Sets the slew step size for the specified load.
  * @param[in]	load	Specifies the load the setting is to be applied to.
- * @param[in]	stp		Specifies the value of the slew step size to be applied (amps or volts).
+ * @param[in]	stp		Specifies the value of the slew step size to be applied (amps).
  * @return				Error status.
  */
 extern Uint16 setLoadSlewStep (loadStage load, float32 stp);
@@ -53,14 +53,14 @@ extern Uint16 scSetLoadStateAll (Uint16 stt);
 
 /** Queries the current slew target setting for the specified channel.
  * @param[in]	load		Specifies the load the setting is to be read from.
- * @param[out]	trgtDest	Address of the memory location at which to place the query result (amps or volts).
+ * @param[out]	trgtDest	Address of the memory location at which to place the query result (amps).
  * @return					Error status.
  */
 extern Uint16 getLoadSlewTarget (loadStage load, float32 * trgtDest);
 
 /** Queries the current slew step size of the specified channel.
  * @param[in]	load	Specifies the load the setting is to be read from.
- * @param[out]	stpDest	Address of the memory location at which to place the query result (amps or volts).
+ * @param[out]	stpDest	Address of the memory location at which to place the query result (amps).
  * @return				Error status.
  */
 extern Uint16 getLoadSlewStep (loadStage load, float32 * stpDest);
