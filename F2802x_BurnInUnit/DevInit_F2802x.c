e8cffa1a47772aac4407cabd3c87662fa2ce2593
Toby <userMe@myServer.fake>
2013-11-01 12:17:47 +0000

    adjusted I2C and GPIO12 initial output values

--- F2802x_BurnInUnit/DevInit_F2802x.c
+++ F2802x_BurnInUnit/DevInit_F2802x.c
@@ -239,16 +239,16 @@ void DeviceInit(void)
 //  GPIO-32 - PIN FUNCTION = I2C-SDA
 	GpioCtrlRegs.GPBMUX1.bit.GPIO32 = 1;	// 0=GPIO,  1=I2C-SDA,  2=SYNCI,  3=ADCSOCA
 	GpioCtrlRegs.GPBDIR.bit.GPIO32 = 0;		// 1=OUTput,  0=INput
-	GpioCtrlRegs.GPBPUD.bit.GPIO32 = 0;    	// Pull-up disabled
+	GpioCtrlRegs.GPBPUD.bit.GPIO32 = 0;    	// Pull-up disable disabled
 //	GpioDataRegs.GPBCLEAR.bit.GPIO32 = 1;	// uncomment if --> Set Low initially
-//	GpioDataRegs.GPBSET.bit.GPIO32 = 1;		// uncomment if --> Set High initially
+	GpioDataRegs.GPBSET.bit.GPIO32 = 1;		// uncomment if --> Set High initially
 //--------------------------------------------------------------------------------------
 //  GPIO-33 - PIN FUNCTION = I2C-SCL
 	GpioCtrlRegs.GPBMUX1.bit.GPIO33 = 1;	// 0=GPIO,  1=I2C-SCL,  2=SYNCO,  3=ADCSOCB
 	GpioCtrlRegs.GPBDIR.bit.GPIO33 = 0;		// 1=OUTput,  0=INput
-	GpioCtrlRegs.GPBPUD.bit.GPIO33 = 0;    	// Pull-up disabled
+	GpioCtrlRegs.GPBPUD.bit.GPIO33 = 0;    	// Pull-up disable disabled
 //	GpioDataRegs.GPBCLEAR.bit.GPIO33 = 1;	// uncomment if --> Set Low initially
-//	GpioDataRegs.GPBSET.bit.GPIO33 = 1;		// uncomment if --> Set High initially
+	GpioDataRegs.GPBSET.bit.GPIO33 = 1;		// uncomment if --> Set High initially
 //--------------------------------------------------------------------------------------
 //  GPIO-34 - PIN FUNCTION = DC Mid VSns comparator (COMP 2) OUT
 	GpioCtrlRegs.GPBMUX1.bit.GPIO34 = 1;	// 0=GPIO,  1=COMP2OUT,  2=EMU1,  3=Resv
