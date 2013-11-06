39fc46873213538ecfb0abc8273f6d146428aa2f
Toby <userMe@myServer.fake>
2013-11-06 12:51:16 +0000

    Added 2 search files to index

---
+++ F2802x_BurnInUnit/Documentation/html/search/defines_6d.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['mid_5fi_5fscale',['MID_I_SCALE',['../a00043.html#a01632f54fd9c0f7d53a0e5c3fdc7c6e2',1,'Settings.h']]],
+  ['mid_5fv_5fr1',['MID_V_R1',['../a00043.html#a08a0080c0df15f8f1aff560c95563e65',1,'Settings.h']]],
+  ['mid_5fv_5fr2',['MID_V_R2',['../a00043.html#a176ea315a270ecc6a601c5b0ce7203b5',1,'Settings.h']]]
+];
